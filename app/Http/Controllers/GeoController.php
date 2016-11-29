bdc7094d2744171954e9bb2c566bf785cc23d79f
Hugo <hugo.pham@hotmail.com>
2016-11-29 11:53:59 -0500

    Models + modified reviews table

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class GeoController extends Controller
 {
-    //
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
 }
--- database/migrations/2016_11_27_040834_create_reviews_table.php
+++ database/migrations/2016_11_27_040834_create_reviews_table.php
@@ -16,10 +16,14 @@ class CreateReviewsTable extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
+            $table->integer('restaurant_id')->unsigned()->index();
             $table->string('title');
             $table->integer('rating');
             $table->text('content');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
         });
     }
 
