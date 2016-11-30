016d32df2e0ebb9425f67b302ccbd8ec9c02cfe2
Hugo <hugo.pham@hotmail.com>
2016-11-30 00:32:45 -0500

    seeding continued

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -7,12 +7,19 @@ use Illuminate\Http\Request;
 class GeoController extends Controller
 {
     /**
+     * Where to redirect users after processing the form.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+
     }
 }
--- database/seeds/RestaurantsTableSeeder.php
+++ database/seeds/RestaurantsTableSeeder.php
@@ -11,14 +11,18 @@ class RestaurantsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('restaurants')->insert([
-            'name' => str_random(10),
-            'address' => str_random(20),
-            'min_price' => random_int(5,10),
-            'max_price' => random_int(20, 50),
-            'latitude' => random_int(30,40),
-            'longitude' => random_int(30),
-            'password' => bcrypt('secret'),
-        ]);
+        $i=0;
+        while($i < 20){
+            DB::table('restaurants')->insert([
+                'user_id' => 1,
+                'name' => str_random(10),
+                'address' => str_random(20),
+                'min_price' => random_int(5,10),
+                'max_price' => random_int(20, 50),
+                'latitude' => mt_rand(45000000, 46000000)/1000000,
+                'longitude' => mt_rand(-74000000,-73000000)/1000000,
+            ]);
+            $i++;
+        }
     }
 }
