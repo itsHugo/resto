578633225c7eb3d498ea08ce69577ca9a678f358
Hugo <hugo.pham@hotmail.com>
2016-12-01 21:58:45 -0500

    Updated redirect

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -15,16 +15,36 @@ class Restaurant extends Model
         'name', 'address', 'genre', 'min_price', 'max_price', 'latitude', 'longitude'
     ];
 
+    /**
+     * Gets the User that created this Restaurant.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo('App\User');
     }
 
+    /**
+     * Determines if a User can edit a Restaurant's information.
+     *
+     * @param User $user
+     * @return bool
+     */
     public function userCanEdit(User $user)
     {
         return $user->id === $this->user_id;
     }
 
+    /**
+     * Defines a one-to-many relationship between a Restaurant and Reviews.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reviews()
+    {
+        return $this->hasMany('App\Review');
+    }
 
 
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,45 +7,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-                    <div class="panel-body">
-                        @if(Auth::check())
-                            You are logged in {{ Auth::user()->name }}!
-                            </br>
-                            Your postal code is {{ Auth::user()->postal_code }}.
-                        @else
+            {{-- Displays a panel to suggest a user to login or register if they are not authenticated. --}}
+            @if(!Auth::check())
+                <div class="panel panel-default">
+                    <div class="panel-heading">Welcome</div>
+                        <div class="panel-body">
                             Please <a href="{{ url('/login') }}">login</a> or
                             <a href="{{ url('/register') }}">register</a>!
-                        @endif
-                    </div>
-                    <div class="panel-body">
-                        <!-- Geolocation -->
-                        <form action="/geo" method="POST" class="form-horizontal" id="hiddenForm">
-                        {{ csrf_field() }}
-                        <!-- Postal code -->
-                            <div class="form-group">
-                                <label for="postal" class="col-sm-3 control-label">Postal Code</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="postal" id="postal" class="form-control" value="{{ old('postal') }}">
-                                </div>
-                            </div>
-                            <!-- all the hidden fields -->
-                            <input type="hidden" name="latitude"/>
-                            <input type="hidden" name="longitude"/>
-                            <input type="hidden" name="error"/>
-                            <!-- submit Button -->
-                            <div class="form-group">
-                                <div class="col-sm-offset-3 col-sm-6">
-                                    <button type="submit" class="btn btn-default">
-                                        <i class="fa fa-btn fa-plus"></i>Submit
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-
-                    </div>
-            </div>
+                        </div>
+                </div>
+            @endif
 
             <!-- Display restaurants -->
             <div class="panel panel-default">
